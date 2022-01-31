697942055f93512ab40a8f4183b880ac7971c4b3
Tyler Gu <jiaweig3@illinois.edu>
2022-01-31 16:17:58 -0600

    parse candidate yaml working

--- Test.py
+++ Test.py
@@ -4,6 +4,7 @@ import os
 import kubernetes
 import yaml
 import time
+import typing
 
 
 def get_deployment_available_status(
@@ -38,8 +39,8 @@ def deploy_operator(operator_yaml_path: str):
     Args:
         operator_yaml_path - path pointing to the operator yaml file
     '''
-    with open(operator_yaml_path,
-              'r') as operator_yaml, open('new_operator.yaml', 'w') as out_yaml:
+    with open(operator_yaml_path,'r') as operator_yaml, \
+            open('new_operator.yaml', 'w') as out_yaml:
         namespace = str()
         parsed_operator_documents = yaml.load_all(operator_yaml,
                                                   Loader=yaml.FullLoader)
@@ -60,19 +61,44 @@ def deploy_operator(operator_yaml_path: str):
 
         print('Deploying the operator, waiting for it to be ready')
         pod_ready = False
-        for _ in range(600):
+        for _ in range(60):
             operator_deployments = appv1.list_namespaced_deployment(
                 namespace, watch=False,
                 label_selector='testing/tag=testing').items
-            if len(operator_deployments
-                  ) >= 1 and get_deployment_available_status(
-                      operator_deployments[0]):
+            if len(operator_deployments) >= 1 \
+                    and get_deployment_available_status(operator_deployments[0]):
                 print('Operator ready')
                 pod_ready = True
                 break
             time.sleep(1)
         if not pod_ready:
-            error("waiting for the operator pod to be ready")
+            error("operator deployment failed to be ready within timeout")
+
+
+def deploy_dependency(yaml_paths):
+    for yaml_path in yaml_paths:
+        os.system('kubectl apply -f %s' % yaml_path)
+    time.sleep(30) # TODO: how to wait smartly
+    return
+
+
+def construct_candidate_helper(node, node_path, result: dict):
+    if 'candidates' in node:
+        result[node_path] = node['candidates']
+    else:
+        for child_key, child_value in node.items():
+            construct_candidate_helper(child_value, '%s.%s' % (node_path, child_key), result)
+
+def construct_candidate_from_yaml(yaml_path: str) -> dict:
+    with open(yaml_path, 'r') as input_yaml:
+        doc = yaml.load(input_yaml, Loader=yaml.FullLoader)
+        result = {}
+        construct_candidate_helper(doc, '', result)
+        return result
+
+
+def mutate_application_spec(current_spec: dict, candidates: dict):
+    return None
 
 
 if __name__ == '__main__':
@@ -80,8 +106,11 @@ if __name__ == '__main__':
 
     construct_kind_cluster()
     deploy_operator('cluster-operator.yml')
+    deploy_dependency([])
+    candidate_dict = construct_candidate_from_yaml('rabbitmq_candidates.yaml')
+    print(candidate_dict)
 
     # while True:
-    #     # TODO: test input generation
+    #     mutate_application_spec()
     #     # TODO: submit to operator
     #     # TODO: check result
