46098833d8ce7ef08f17c632b239a27c4d1916ed
Tyler Gu <jiaweig3@illinois.edu>
2022-04-06 14:19:02 -0500

    misc: prune toleration field in rabbitmq

--- rabbitmq.py
+++ rabbitmq.py
@@ -34,7 +34,24 @@ class AffinitySchema(schema.ObjectSchema):
         return 'affinity'
 
 
+class TolerationSchema(schema.ObjectSchema):
+
+    def __init__(self, path: list, schema: dict) -> None:
+        super().__init__(path, schema)
+
+    def __init__(self, schema_obj: schema.BaseSchema) -> None:
+        super().__init__(schema_obj.path, schema_obj.raw_schema)
+
+    def get_all_schemas(self) -> list:
+        '''Return all the subschemas as a list'''
+        return [self]
+
+    def __str__(self) -> str:
+        return 'toleration'
+
+
 custom_fields = [
     input.CustomField(['spec', 'override'], OverrideSchema),
-    input.CustomField(['spec', 'affinity'], AffinitySchema)
+    input.CustomField(['spec', 'affinity'], AffinitySchema),
+    input.CustomField(['spec', 'tolerations'], TolerationSchema)
 ]
