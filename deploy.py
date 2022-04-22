2b7ec9bb460ef458fb91a1d7e7b41d1feade3738
Kuan-Yin Chen <git@kuanyin.dev>
2022-04-22 01:23:30 +0000

    clean code

--- deploy.py
+++ deploy.py
@@ -151,7 +151,6 @@ class Kustomize(Deploy):
                 sh.kubectl("apply", filename=self.init_yaml)
             sleep(self.wait)
             sh.kubectl("apply", "--force-conflicts", "--server-side",  "-k", self.path)
-            # os.system("kubectl apply --force-conflicts --server-side -k 'github.com/k8ssandra/cass-operator/config/deployments/cluster?ref=v1.10.3'")
             super().check_status()
             return True
         except Exception:
