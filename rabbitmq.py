7388d115c600bce995efb624a552e4b5276f6ad6
Kuan-Yin Chen <git@kuanyin.dev>
2022-04-22 01:22:58 +0000

    revert

--- rabbitmq.py
+++ rabbitmq.py
@@ -50,24 +50,8 @@ class TolerationSchema(schema.ObjectSchema):
         return 'toleration'
 
 
-class PodtemplatespecSchema(schema.ObjectSchema):
-
-    def __init__(self, path: list, schema: dict) -> None:
-        super().__init__(path, schema)
-
-    def __init__(self, schema_obj: schema.BaseSchema) -> None:
-        super().__init__(schema_obj.path, schema_obj.raw_schema)
-
-    def get_all_schemas(self) -> list:
-        '''Return all the subschemas as a list'''
-        return [self]
-
-    def __str__(self) -> str:
-        return 'podTemplateSpec'
-
-
 custom_fields = [
-    # input.CustomField(['spec', 'override'], OverrideSchema),
-    # input.CustomField(['spec', 'affinity'], AffinitySchema),
-    input.CustomField(['spec', 'podTemplateSpec'], PodtemplatespecSchema)
+    input.CustomField(['spec', 'override'], OverrideSchema),
+    input.CustomField(['spec', 'affinity'], AffinitySchema),
+    input.CustomField(['spec', 'tolerations'], TolerationSchema)
 ]
