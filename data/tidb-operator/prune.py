7dd1c3978c27ef84de39a65ea8f03fb294b432bd
Shuai Wang <shuaiwang516@gmail.com>
2022-05-08 22:38:20 -0500

    add crd and prune:

---
+++ data/tidb-operator/prune.py
@@ -0,0 +1,47 @@
+import sys
+
+sys.path.append('../..')
+import input
+
+custom_fields = [
+    input.CopiedOverField(['spec', 'discovery', 'initContainers'], True),
+    input.CopiedOverField(['spec', 'discovery', 'affinity']),
+    input.CopiedOverField(['spec', 'discovery', 'tolerations'], True),
+    input.CopiedOverField(['spec', 'pd', 'additionalContainers'], True),
+    input.CopiedOverField(['spec', 'pd', 'additionalVolumes'], True),
+    input.CopiedOverField(['spec', 'pd', 'affinity']),
+    input.CopiedOverField(['spec', 'pd', 'initContainers'], True),
+    input.CopiedOverField(['spec', 'pd', 'tolerations'], True),
+    input.CopiedOverField(['spec', 'podSecurityContext']),
+    input.CopiedOverField(['spec', 'pump', 'additionalContainers'], True),
+    input.CopiedOverField(['spec', 'pump', 'additionalVolumes'], True),
+    input.CopiedOverField(['spec', 'pump', 'affinity']),
+    input.CopiedOverField(['spec', 'pump', 'initContainers'], True),
+    input.CopiedOverField(['spec', 'pump', 'tolerations'], True),
+    input.CopiedOverField(['spec', 'pump', 'podSecurityContext']),
+    input.CopiedOverField(['spec', 'ticdc', 'additionalContainers'], True),
+    input.CopiedOverField(['spec', 'ticdc', 'additionalVolumes'], True),
+    input.CopiedOverField(['spec', 'ticdc', 'affinity']),
+    input.CopiedOverField(['spec', 'ticdc', 'initContainers'], True),
+    input.CopiedOverField(['spec', 'ticdc', 'tolerations'], True),
+    input.CopiedOverField(['spec', 'ticdc', 'podSecurityContext']), 
+    input.CopiedOverField(['spec', 'tidb', 'additionalContainers'], True),
+    input.CopiedOverField(['spec', 'tidb', 'additionalVolumes'], True),
+    input.CopiedOverField(['spec', 'tidb', 'affinity']),
+    input.CopiedOverField(['spec', 'tidb', 'initContainers'], True),
+    input.CopiedOverField(['spec', 'tidb', 'tolerations'], True),
+    input.CopiedOverField(['spec', 'tidb', 'podSecurityContext']),
+    input.CopiedOverField(['spec', 'tiflash', 'additionalContainers'], True),
+    input.CopiedOverField(['spec', 'tiflash', 'additionalVolumes'], True),
+    input.CopiedOverField(['spec', 'tiflash', 'affinity']),
+    input.CopiedOverField(['spec', 'tiflash', 'initContainers'], True),
+    input.CopiedOverField(['spec', 'tiflash', 'tolerations'], True),
+    input.CopiedOverField(['spec', 'tiflash', 'podSecurityContext']),
+    input.CopiedOverField(['spec', 'tikv', 'additionalContainers'], True),
+    input.CopiedOverField(['spec', 'tikv', 'additionalVolumes'], True),
+    input.CopiedOverField(['spec', 'tikv', 'affinity']),
+    input.CopiedOverField(['spec', 'tikv', 'initContainers'], True),
+    input.CopiedOverField(['spec', 'tikv', 'tolerations'], True),
+    input.CopiedOverField(['spec', 'tikv', 'podSecurityContext']),
+    input.CopiedOverField(['spec', 'tolerations'], True)
+]
