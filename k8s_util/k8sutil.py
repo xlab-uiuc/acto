ee13025e6b2adee3781daf12afae1a96abf312e8
yuxuan <yuxuanj9@illinois.edu>
2022-06-16 16:03:44 -0500

    add: unit n and u into canonicalizeQuantity's regex

--- k8s_util/k8sutil.py
+++ k8s_util/k8sutil.py
@@ -2,7 +2,7 @@ import ctypes
 import re
 
 def canonicalizeQuantity(value):
-    if not isinstance(value, str) or not bool(re.match('^[-+]?((\.[0-9]+)|([0-9]+(\.[0-9]+)?)|([0-9]+\.))(([KMGTPE]i)|([eE][-+]?((\.[0-9]+)|([0-9](\.[0-9]+)?)|([0-9]+\.)))|([mkMGTPE]|))$', value)):
+    if not isinstance(value, str) or not bool(re.match('^[-+]?((\.[0-9]+)|([0-9]+(\.[0-9]+)?)|([0-9]+\.))(([KMGTPE]i)|([eE][-+]?((\.[0-9]+)|([0-9](\.[0-9]+)?)|([0-9]+\.)))|([unmkMGTPE]|))$', value)):
         return value
     k8sutil = ctypes.cdll.LoadLibrary('k8s_util/lib/k8sutil.so')
     parse = k8sutil.parse
@@ -18,3 +18,5 @@ if __name__ == '__main__':
     print(canonicalizeQuantity('172.18.0.4'))
     print(canonicalizeQuantity('1Mi'))
     print(canonicalizeQuantity('asd'))
+    print(canonicalizeQuantity('500m'))
+    print(canonicalizeQuantity('10000000u'))
