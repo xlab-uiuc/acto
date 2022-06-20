613bb1fe85ffc371360ebdfc980a0da3f4951aa2
yuxuan <yuxuanj9@illinois.edu>
2022-06-19 21:18:42 -0500

    update README.md

---
+++ data/percona-xtradb-cluster-operator/prune.py
@@ -0,0 +1,33 @@
+import input
+
+custom_fields = [
+    input.CopiedOverField(['spec', 'haproxy', 'affinity']),
+    input.CopiedOverField(['spec', 'haproxy', 'containerSecurityContext']),
+    input.CopiedOverField(['spec', 'haproxy', 'livenessProbes']),
+    input.CopiedOverField(['spec', 'haproxy', 'readinessProbes']),
+    input.CopiedOverField(['spec', 'haproxy', 'tolerations']),
+    input.CopiedOverField(['spec', 'haproxy', 'sidecars'], True),
+    input.CopiedOverField(['spec', 'haproxy', 'sidecarVolumes'], True),
+    input.CopiedOverField(['spec', 'haproxy', 'sidecarPVCs'], True),
+    input.CopiedOverField(['spec', 'haproxy', 'volumeSpec']),
+
+    input.CopiedOverField(['spec', 'proxysql', 'affinity']),
+    input.CopiedOverField(['spec', 'proxysql', 'containerSecurityContext']),
+    input.CopiedOverField(['spec', 'proxysql', 'livenessProbes']),
+    input.CopiedOverField(['spec', 'proxysql', 'readinessProbes']),
+    input.CopiedOverField(['spec', 'proxysql', 'tolerations']),
+    input.CopiedOverField(['spec', 'proxysql', 'sidecars'], True),
+    input.CopiedOverField(['spec', 'proxysql', 'sidecarVolumes'], True),
+    input.CopiedOverField(['spec', 'proxysql', 'sidecarPVCs'], True),
+    input.CopiedOverField(['spec', 'proxysql', 'volumeSpec']),
+
+    input.CopiedOverField(['spec', 'pxc', 'affinity']),
+    input.CopiedOverField(['spec', 'pxc', 'containerSecurityContext']),
+    input.CopiedOverField(['spec', 'pxc', 'livenessProbes']),
+    input.CopiedOverField(['spec', 'pxc', 'readinessProbes']),
+    input.CopiedOverField(['spec', 'pxc', 'tolerations']),
+    input.CopiedOverField(['spec', 'pxc', 'sidecars'], True),
+    input.CopiedOverField(['spec', 'pxc', 'sidecarVolumes'], True),
+    input.CopiedOverField(['spec', 'pxc', 'sidecarPVCs'], True),
+    input.CopiedOverField(['spec', 'pxc', 'volumeSpec']),
+]
