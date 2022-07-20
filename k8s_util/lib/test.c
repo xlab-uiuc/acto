7139a683a9253d1d3652ad0937757e295fe2e110
unw9527 <1041593558@qq.com>
2022-07-20 16:07:13 -0500

    disable unused gates

--- k8s_util/lib/test.c
+++ k8s_util/lib/test.c
@@ -2,7 +2,7 @@
 #include "k8sutil.h"
 
 int main(){
-    char *name = "-92743e6047801799";
+    char *name = "92743e6047801799";
     printf("The string after parsing is: %s\n", parse(name));
     return 0;
 }
