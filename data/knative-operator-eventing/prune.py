f34929abf7ad4a2996c6f8fdf8c1f98624dabd5b
Yuxuan <yuxuanj9@illinois.edu>
2022-08-23 19:53:08 +0800

    prune external deps for knative-operator

---
+++ data/knative-operator-eventing/prune.py
@@ -0,0 +1,5 @@
+import input
+
+custom_fields = [
+    input.ProblematicField(['spec', 'registry', 'override']),
+]
---
+++ data/knative-operator-serving/prune.py
@@ -0,0 +1,6 @@
+import input
+
+custom_fields = [
+    input.ProblematicField(['spec', 'ingress', 'istio', 'enabled']),
+    input.ProblematicField(['spec', 'registry', 'override']),
+]
