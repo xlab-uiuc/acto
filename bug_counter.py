e9010415c1ed97066cf4d7877284f933d6387e28
unw9527 <1041593558@qq.com>
2022-08-24 16:20:56 +0800

    add: auto counting the number of bugs in bugs.md

---
+++ bug_counter.py
@@ -0,0 +1,32 @@
+num_bugs = 0
+num_confirmed = 0
+num_fixed = 0
+num_by_bugs = 0
+num_by_confirmed = 0
+num_by_fixed = 0
+by_flag = False
+
+with open ('./bugs.md', 'r') as f:
+    data = f.readlines()
+    
+
+for line in data:
+    if '# Byproduct bugs' in line:
+        by_flag = True
+    if not by_flag:
+        num_bugs += line.count('](')
+        num_confirmed += line.count('confirmed')
+        num_fixed += line.count('fixed')
+    else:
+        num_by_bugs += line.count('](')
+        num_by_confirmed += line.count('confirmed')
+        num_by_fixed += line.count('fixed')
+        
+num_confirmed += num_fixed
+num_by_confirmed += num_by_fixed
+data[1] = '(Byproduct bugs included) Total bugs: **{}**, confirmed: **{}** , fixed: **{}**.<br/>\n'.format(num_bugs + num_by_bugs, num_confirmed + num_by_confirmed, num_fixed + num_by_fixed)
+data[3] = '(Byproduct bugs excluded) Total bugs: **{}**, confirmed: **{}** , fixed: **{}**.<br/>\n'.format(num_bugs, num_confirmed, num_fixed)
+
+with open ('./bugs.md', 'w') as f:
+    f.writelines(data)
+    
