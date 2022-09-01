0f8b653fefa34a2b45aaa0b2d4c1e106f5a5823f
Yuxuan <yuxuanj9@illinois.edu>
2022-09-01 10:27:00 +0800

    fix: rounding up Quantity for coreV1 compatiability
    Signed-off-by: Yuxuan <yuxuanj9@illinois.edu>

--- compare.py
+++ compare.py
@@ -116,25 +116,25 @@ if __name__ == '__main__':
         #     '4lw.commands.whitelist=cons, envi, conf, crst, srvr, stat, mntr, ruok\ndataDir=/data\nstandaloneEnabled=false\nreconfigEnabled=true\nskipACL=yes\nmetricsProvider.className=org.apache.zookeeper.metrics.prometheus.PrometheusMetricsProvider\nmetricsProvider.httpPort=7000\nmetricsProvider.exportJvmInfo=true\ninitLimit=10\nsyncLimit=2\ntickTime=2000\nglobalOutstandingLimit=1000\npreAllocSize=65536\nsnapCount=10000\ncommitLogCount=500\nsnapSizeLimitInKb=4194304\nmaxCnxns=0\nmaxClientCnxns=60\nminSessionTimeout=4000\nmaxSessionTimeout=40000\nautopurge.snapRetainCount=3\nautopurge.purgeInterval=1\nquorumListenOnAllIPs=false\nadmin.serverPort=8080\ndynamicConfigFile=/data/zoo.cfg.dynamic\n',
         #     'apqwpwxmlo=kcaqbdpkpt\n4lw.commands.whitelist=cons, envi, conf, crst, srvr, stat, mntr, ruok\ndataDir=/data\nstandaloneEnabled=false\nreconfigEnabled=true\nskipACL=yes\nmetricsProvider.className=org.apache.zookeeper.metrics.prometheus.PrometheusMetricsProvider\nmetricsProvider.httpPort=7000\nmetricsProvider.exportJvmInfo=true\ninitLimit=10\nsyncLimit=4\ntickTime=2000\nglobalOutstandingLimit=1000\npreAllocSize=2\nsnapCount=5\ncommitLogCount=2\nsnapSizeLimitInKb=4194304\nmaxCnxns=0\nmaxClientCnxns=60\nminSessionTimeout=5\nmaxSessionTimeout=5\nautopurge.snapRetainCount=3\nautopurge.purgeInterval=5\nquorumListenOnAllIPs=true\nadmin.serverPort=8080\ndynamicConfigFile=/data/zoo.cfg.dynamic\n'
         # ],
-        # [
-        #     'cluster_partition_handling = pause_minority\nvm_memory_high_watermark_paging_ratio = 0.99\ndisk_free_limit.relative = 1.0\ncollect_statistics_interval = 10000\n',
-        #     None,
-        #     'total_memory_available_override_value = 3435973837\ncluster_partition_handling            = pause_minority\nvm_memory_high_watermark_paging_ratio = 0.99\ndisk_free_limit.relative              = 1.0\ncollect_statistics_interval           = 10000\n',
-        #     None
-        # ],
-        # [
-        #     None, '<your-password-here-new>',
-        #     'PHlvdXItcGFzc3dvcmQtaGVyZT4K', 'PHlvdXItcGFzc3dvcmQtaGVyZS1uZXc+Cg=='
-
-        # ],
-        # [
-        #     None, "-.4272625998Mi",
-        #     None, "-448017308m"
-        # ],
+        [
+            'cluster_partition_handling = pause_minority\nvm_memory_high_watermark_paging_ratio = 0.99\ndisk_free_limit.relative = 1.0\ncollect_statistics_interval = 10000\n',
+            None,
+            'total_memory_available_override_value = 3435973837\ncluster_partition_handling            = pause_minority\nvm_memory_high_watermark_paging_ratio = 0.99\ndisk_free_limit.relative              = 1.0\ncollect_statistics_interval           = 10000\n',
+            None
+        ],
+        [
+            None, "-.4272625998Mi",
+            None, "-448017308m"
+        ],
         [
             None, 10,
             None, 1000
-        ] # this case is used to test substring matching
+        ],  # this case is used to test substring matching
+        [
+            None, '.1571549',
+            None, '158m'
+        ]
+
     ]
     compare = CompareMethods()
 
--- k8s_util/lib/k8sutil.go
+++ k8s_util/lib/k8sutil.go
@@ -15,6 +15,9 @@ func parse(valuePtr *C.char) *C.char {
 		return C.CString("INVALID")
 	}
 	// return C.CString(strconv.FormatInt(q.Value(), 10))
+	const milliScale = -3
+	q.RoundUp(milliScale) // round up to the nearest milli for V1Api compatiability
+
 	return C.CString(q.AsDec().String())
 }
 
@@ -25,5 +28,6 @@ func main() {
 	if err != nil {
 		fmt.Printf("Error in converting the string %v", err)
 	}
-	fmt.Println(ans.Value())
+	fmt.Print(ans.RoundUp(-3))
+	fmt.Printf("The value is %v", ans)
 }
