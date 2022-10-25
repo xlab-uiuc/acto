1a2b0f0aa7e0250290156c7e1bd6540b7f9e3700
Tyler Gu <jiaweig3@illinois.edu>
2022-10-25 16:32:14 -0500

    feat: add k9s to ansible script

--- scripts/ansible/configure.sh
+++ scripts/ansible/configure.sh
@@ -8,5 +8,6 @@ ansible-playbook kubectl.yaml -i ansible_hosts
 ansible-playbook helm.yaml -i ansible_hosts
 ansible-playbook sysctl.yaml -i ansible_hosts
 ansible-playbook k3d.yaml -i ansible_hosts
+ansible-playbook k9s.yaml -i ansible_hosts
 ansible-playbook htop.yaml -i ansible_hosts
 ansible-playbook acto.yaml -i ansible_hosts
