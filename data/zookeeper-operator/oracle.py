fd798c3c382d930b23805954edd8d6bfd821ed11
Tyler Gu <jiaweig3@illinois.edu>
2022-11-01 14:29:36 -0500

    fix: indentation error

--- data/zookeeper-operator/oracle.py
+++ data/zookeeper-operator/oracle.py
@@ -18,21 +18,28 @@ def zookeeper_checker(handle: OracleHandle) -> RunResult:
     leaders = 0
 
     for pod in pod_list:
-    	if pod.status.pod_ip == None:
-	    return ErrorResult(oracle=Oracle.CUSTOM, msg='Zookeeper pod does not have an IP assigned')
+        if pod.status.pod_ip == None:
+            return ErrorResult(oracle=Oracle.CUSTOM,
+                               msg='Zookeeper pod does not have an IP assigned')
         p = handle.kubectl_client.exec(
-            pod.metadata.name, pod.metadata.namespace,
-            ['curl', 'http://' + pod.status.pod_ip + ':8080/commands/ruok'], capture_output=True, text=True)
+            pod.metadata.name,
+            pod.metadata.namespace, ['curl', 'http://' + pod.status.pod_ip + ':8080/commands/ruok'],
+            capture_output=True,
+            text=True)
         result = json.loads(p.stdout)
         if result['error'] != None:
-            return ErrorResult(oracle=Oracle.CUSTOM, msg='Zookeeper cluster curl has error')
+            return ErrorResult(oracle=Oracle.CUSTOM,
+                               msg='Zookeeper cluster curl has error ' + result['error'])
 
         p = handle.kubectl_client.exec(
-            pod.metadata.name, pod.metadata.namespace,
-            ['curl', 'http://' + pod.status.pod_ip + ':8080/commands/stat'], capture_output=True, text=True)
+            pod.metadata.name,
+            pod.metadata.namespace, ['curl', 'http://' + pod.status.pod_ip + ':8080/commands/stat'],
+            capture_output=True,
+            text=True)
         result = json.loads(p.stdout)
         if result['error'] != None:
-            return ErrorResult(oracle=Oracle.CUSTOM, msg='Zookeeper cluster curl has error')
+            return ErrorResult(oracle=Oracle.CUSTOM,
+                               msg='Zookeeper cluster curl has error ' + result['error'])
         elif result['server_stats']['server_state'] == 'leader':
             leaders += 1
 
