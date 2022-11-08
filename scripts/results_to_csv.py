7e491ece5cc9bedcabe8c944b106c0e62a23e3b2
Tyler Gu <jiaweig3@illinois.edu>
2022-11-07 18:48:57 -0600

    add: new script for transform results to csv

--- scripts/results_to_csv.py
+++ scripts/results_to_csv.py
@@ -8,25 +8,13 @@ import pandas
 if __name__ == '__main__':
     result_folder = sys.argv[1]
     json_paths = glob.glob(os.path.join(result_folder, '*', 'post_result.json'))
-    recovery_paths = glob.glob(os.path.join(result_folder, '*', 'recovery_result.json'))
 
     with open(os.path.join(result_folder, 'result.csv'), 'w') as result_file:
         writer = csv.writer(result_file, delimiter=',')
         writer.writerow([
-            'Trial number', 'Original Oracle type', 'Original Message', 'Original Input path',
-            'Post Oracle type', 'Post Message', 'Post Input path', 'True/False alarm', 'Category',
-            'Comment'
+            'Trial number', 'Crash Result', 'Recovery Result', 'Original State Result',
+            'Post State Result', 'Custom Result', 'True/False alarm', 'Category', 'Comment'
         ])
-
-        for recovery_path in recovery_paths:
-            with open(recovery_path, 'r') as recovery_file:
-                recovery_result = json.load(recovery_file)
-
-                writer.writerow([
-                    '%s' % recovery_path, 'Recovery',
-                    '%s' % recovery_result['delta'], '', 'Recovery',
-                    '%s' % recovery_result['delta'], ''
-                ])
 
         for json_path in json_paths:
             with open(json_path, 'r') as json_file:
@@ -34,24 +22,60 @@ if __name__ == '__main__':
 
                 original_result = json_instance['original_result']
                 post_result = json_instance['post_result']
-                if 'input_delta' not in original_result or original_result['input_delta'] == None:
-                    original_path = None
-                else:
-                    original_path = original_result['input_delta']['path']
+                if 'error' in original_result:
+                    if original_result['error']['crash_result'] == None:
+                        crash_result = 'Pass'
+                    elif isinstance(original_result['error']['crash_result'], dict):
+                        crash_result = original_result['error']['crash_result']['message']
+                    elif original_result['error']['crash_result'] == 'Pass':
+                        crash_result = 'Pass'
 
-                if 'input_delta' not in post_result or post_result['input_delta'] == None:
-                    post_path = None
+                    if original_result['error']['recovery_result'] == None:
+                        recovery_result = 'Pass'
+                    elif isinstance(original_result['error']['recovery_result'], dict):
+                        recovery_result = original_result['error']['recovery_result']['delta']
+                    elif original_result['error']['recovery_result'] == 'Pass':
+                        recovery_result = 'Pass'
+
+                    if original_result['error']['custom_result'] == None:
+                        custom_result = 'Pass'
+                    elif isinstance(original_result['error']['custom_result'], dict):
+                        custom_result = original_result['error']['custom_result']['message']
+                    elif original_result['error']['custom_result'] == 'Pass':
+                        custom_result = 'Pass'
+
+                    if original_result['error']['state_result'] == None:
+                        original_state_result = 'Pass'
+                    elif isinstance(original_result['error']['state_result'], dict):
+                        original_state_result = original_result['error']['state_result'][
+                            'input_delta']['path']
+                    elif original_result['error']['state_result'] == 'Pass':
+                        original_state_result = 'Pass'
                 else:
-                    post_path = post_result['input_delta']['path']
+                    crash_result = 'Pass'
+                    recovery_result = 'Pass'
+                    original_state_result = 'Pass'
+                    custom_result = 'Pass'
+
+                if 'error' in post_result:
+
+                    if post_result['error']['state_result'] == None:
+                        post_state_result = 'Pass'
+                    elif isinstance(post_result['error']['state_result'], dict):
+                        post_state_result = post_result['error']['state_result']['input_delta'][
+                            'path']
+                    elif post_result['error']['state_result'] == 'Pass':
+                        post_state_result = 'Pass'
+                else:
+                    post_state_result = 'Pass'
 
                 writer.writerow([
                     '%s' % original_result['trial_num'],
-                    None if 'oracle' not in original_result else original_result['oracle'],
-                    None if 'message' not in original_result else original_result['message'],
-                    original_path,
-                    None if 'oracle' not in post_result else post_result['oracle'],
-                    None if 'message' not in post_result else post_result['message'],
-                    post_path,
+                    '%s' % crash_result,
+                    '%s' % recovery_result,
+                    '%s' % original_state_result,
+                    '%s' % post_state_result,
+                    '%s' % custom_result,
                 ])
 
     df = pandas.read_csv(os.path.join(result_folder, 'result.csv'))
