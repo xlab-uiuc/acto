105f998fb1bee84cd76fc747372336f0e20b6062
Tyler Gu <jiaweig3@illinois.edu>
2022-11-09 12:15:35 -0600

    Update script to aggreate results in xlsx

--- scripts/feature_results_to_csv.py
+++ scripts/feature_results_to_csv.py
@@ -25,14 +25,44 @@ if __name__ == '__main__':
         with open(json_path, 'r') as json_file:
             json_instance = json.load(json_file)
 
+            post_result = json_instance['post_result']
+            if post_result['error']['crash_result'] == None:
+                crash_result = 'Pass'
+            elif isinstance(post_result['error']['crash_result'], dict):
+                crash_result = post_result['error']['crash_result']['message']
+            elif post_result['error']['crash_result'] == 'Pass':
+                crash_result = 'Pass'
+
+            if post_result['error']['state_result'] == None:
+                post_state_result = 'Pass'
+            elif isinstance(post_result['error']['state_result'], dict):
+                post_state_result = post_result['error']['state_result']['input_delta'][
+                    'path']
+            elif post_result['error']['state_result'] == 'Pass':
+                post_state_result = 'Pass'
+
+            if post_result['error']['recovery_result'] == None:
+                recovery_result = 'Pass'
+            elif isinstance(post_result['error']['recovery_result'], dict):
+                recovery_result = post_result['error']['recovery_result']['delta']
+            elif post_result['error']['recovery_result'] == 'Pass':
+                recovery_result = 'Pass'
+
+            if post_result['error']['custom_result'] == None:
+                custom_result = 'Pass'
+            elif isinstance(post_result['error']['custom_result'], dict):
+                custom_result = post_result['error']['custom_result']['message']
+            elif post_result['error']['custom_result'] == 'Pass':
+                custom_result = 'Pass'
+
             baseline_df = baseline_df.append(
                 {
                     'Trial number': json_instance['post_result']['trial_num'],
                     'baseline_alarm': json_instance['alarm'],
-                    'baseline_crash_result': json_instance['post_result']['error']['crash_result'],
-                    'baseline_recovery_result': json_instance['post_result']['error']['recovery_result'],
-                    'baseline_state_result': json_instance['post_result']['error']['state_result'],
-                    'baseline_custom_result': json_instance['post_result']['error']['custom_result']
+                    'baseline_crash_result': crash_result,
+                    'baseline_recovery_result': recovery_result,
+                    'baseline_state_result': post_state_result,
+                    'baseline_custom_result': custom_result
                 },
                 ignore_index=True)
 
@@ -40,14 +70,44 @@ if __name__ == '__main__':
         with open(json_path, 'r') as json_file:
             json_instance = json.load(json_file)
 
+            post_result = json_instance['post_result']
+            if post_result['error']['crash_result'] == None:
+                crash_result = 'Pass'
+            elif isinstance(post_result['error']['crash_result'], dict):
+                crash_result = post_result['error']['crash_result']['message']
+            elif post_result['error']['crash_result'] == 'Pass':
+                crash_result = 'Pass'
+
+            if post_result['error']['state_result'] == None:
+                post_state_result = 'Pass'
+            elif isinstance(post_result['error']['state_result'], dict):
+                post_state_result = post_result['error']['state_result']['input_delta'][
+                    'path']
+            elif post_result['error']['state_result'] == 'Pass':
+                post_state_result = 'Pass'
+
+            if post_result['error']['recovery_result'] == None:
+                recovery_result = 'Pass'
+            elif isinstance(post_result['error']['recovery_result'], dict):
+                recovery_result = post_result['error']['recovery_result']['delta']
+            elif post_result['error']['recovery_result'] == 'Pass':
+                recovery_result = 'Pass'
+
+            if post_result['error']['custom_result'] == None:
+                custom_result = 'Pass'
+            elif isinstance(post_result['error']['custom_result'], dict):
+                custom_result = post_result['error']['custom_result']['message']
+            elif post_result['error']['custom_result'] == 'Pass':
+                custom_result = 'Pass'
+
             canonicalization_df = canonicalization_df.append(
                 {
                     'Trial number': json_instance['post_result']['trial_num'],
                     'canonicalization_alarm': json_instance['alarm'],
-                    'canonicalization_crash_result': json_instance['post_result']['error']['crash_result'],
-                    'canonicalization_recovery_result': json_instance['post_result']['error']['recovery_result'],
-                    'canonicalization_state_result': json_instance['post_result']['error']['state_result'],
-                    'canonicalization_custom_result': json_instance['post_result']['error']['custom_result']
+                    'canonicalization_crash_result': crash_result,
+                    'canonicalization_recovery_result': recovery_result,
+                    'canonicalization_state_result': post_state_result,
+                    'canonicalization_custom_result': custom_result
                 },
                 ignore_index=True)
 
@@ -55,14 +115,44 @@ if __name__ == '__main__':
         with open(json_path, 'r') as json_file:
             json_instance = json.load(json_file)
 
+            post_result = json_instance['post_result']
+            if post_result['error']['crash_result'] == None:
+                crash_result = 'Pass'
+            elif isinstance(post_result['error']['crash_result'], dict):
+                crash_result = post_result['error']['crash_result']['message']
+            elif post_result['error']['crash_result'] == 'Pass':
+                crash_result = 'Pass'
+
+            if post_result['error']['state_result'] == None:
+                post_state_result = 'Pass'
+            elif isinstance(post_result['error']['state_result'], dict):
+                post_state_result = post_result['error']['state_result']['input_delta'][
+                    'path']
+            elif post_result['error']['state_result'] == 'Pass':
+                post_state_result = 'Pass'
+
+            if post_result['error']['recovery_result'] == None:
+                recovery_result = 'Pass'
+            elif isinstance(post_result['error']['recovery_result'], dict):
+                recovery_result = post_result['error']['recovery_result']['delta']
+            elif post_result['error']['recovery_result'] == 'Pass':
+                recovery_result = 'Pass'
+
+            if post_result['error']['custom_result'] == None:
+                custom_result = 'Pass'
+            elif isinstance(post_result['error']['custom_result'], dict):
+                custom_result = post_result['error']['custom_result']['message']
+            elif post_result['error']['custom_result'] == 'Pass':
+                custom_result = 'Pass'
+
             dependency_df = dependency_df.append(
                 {
                     'Trial number': json_instance['post_result']['trial_num'],
                     'dependency_alarm': json_instance['alarm'],
-                    'dependency_crash_result': json_instance['post_result']['error']['crash_result'],
-                    'dependency_recovery_result': json_instance['post_result']['error']['recovery_result'],
-                    'dependency_state_result': json_instance['post_result']['error']['state_result'],
-                    'dependency_custom_result': json_instance['post_result']['error']['custom_result']
+                    'dependency_crash_result': crash_result,
+                    'dependency_recovery_result': recovery_result,
+                    'dependency_state_result': post_state_result,
+                    'dependency_custom_result': custom_result
                 },
                 ignore_index=True)
 
@@ -70,14 +160,44 @@ if __name__ == '__main__':
         with open(json_path, 'r') as json_file:
             json_instance = json.load(json_file)
 
+            post_result = json_instance['post_result']
+            if post_result['error']['crash_result'] == None:
+                crash_result = 'Pass'
+            elif isinstance(post_result['error']['crash_result'], dict):
+                crash_result = post_result['error']['crash_result']['message']
+            elif post_result['error']['crash_result'] == 'Pass':
+                crash_result = 'Pass'
+
+            if post_result['error']['state_result'] == None:
+                post_state_result = 'Pass'
+            elif isinstance(post_result['error']['state_result'], dict):
+                post_state_result = post_result['error']['state_result']['input_delta'][
+                    'path']
+            elif post_result['error']['state_result'] == 'Pass':
+                post_state_result = 'Pass'
+
+            if post_result['error']['recovery_result'] == None:
+                recovery_result = 'Pass'
+            elif isinstance(post_result['error']['recovery_result'], dict):
+                recovery_result = post_result['error']['recovery_result']['delta']
+            elif post_result['error']['recovery_result'] == 'Pass':
+                recovery_result = 'Pass'
+
+            if post_result['error']['custom_result'] == None:
+                custom_result = 'Pass'
+            elif isinstance(post_result['error']['custom_result'], dict):
+                custom_result = post_result['error']['custom_result']['message']
+            elif post_result['error']['custom_result'] == 'Pass':
+                custom_result = 'Pass'
+
             taint_analysis_df = taint_analysis_df.append(
                 {
                     'Trial number': json_instance['post_result']['trial_num'],
                     'taint_analysis_alarm': json_instance['alarm'],
-                    'taint_analysis_crash_result': json_instance['post_result']['error']['crash_result'],
-                    'taint_analysis_recovery_result': json_instance['post_result']['error']['recovery_result'],
-                    'taint_analysis_state_result': json_instance['post_result']['error']['state_result'],
-                    'taint_analysis_custom_result': json_instance['post_result']['error']['custom_result']
+                    'taint_analysis_crash_result': crash_result,
+                    'taint_analysis_recovery_result': recovery_result,
+                    'taint_analysis_state_result': post_state_result,
+                    'taint_analysis_custom_result': custom_result
                 },
                 ignore_index=True)
 
@@ -87,11 +207,12 @@ if __name__ == '__main__':
     merged= merged.drop(columns=list(merged.filter(regex = '_result')))
     merged = merged.sort_values(by=['Trial number'])
 
-    merged.to_csv(os.path.join(result_folder, 'merged.csv'), index=False)
-    baseline_df.to_csv(os.path.join(result_folder, 'baseline.csv'), index=False)
-    canonicalization_df.to_csv(os.path.join(result_folder, 'canonicalization.csv'), index=False)
-    dependency_df.to_csv(os.path.join(result_folder, 'dependency.csv'), index=False)
-    taint_analysis_df.to_csv(os.path.join(result_folder, 'taint_analysis.csv'), index=False)
+    with pd.ExcelWriter(os.path.join(result_folder, 'result.xlsx'), mode='w') as writer:
+        merged.to_excel(writer, sheet_name='Overview', index=False)
+        baseline_df.to_excel(writer, sheet_name='Baseline', index=False)
+        canonicalization_df.to_excel(writer, sheet_name='Canonicalization', index=False)
+        dependency_df.to_excel(writer, sheet_name='Dependency', index=False)
+        taint_analysis_df.to_excel(writer, sheet_name='Taint analysis', index=False)
 
     print(merged['baseline_alarm'].value_counts())
     print(merged['canonicalization_alarm'].value_counts())
