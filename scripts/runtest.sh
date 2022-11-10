8082f41c9d209c6ac4fd0fa2a6b58e85bcd5cb88
Tyler Gu <jiaweig3@illinois.edu>
2022-11-09 21:03:45 -0600

    feat: automation script

--- scripts/runtest.sh
+++ scripts/runtest.sh
@@ -41,3 +41,6 @@ bash scripts/teardown.sh
 python3 acto.py --config data/zookeeper-operator/config.json --num-workers 16 --num-cases 1 --workdir testrun-zk --notify-crash
 python3 checker.py --config data/zookeeper-operator/config.json --num-workers 16 --testrun-dir testrun-zk
 bash scripts/teardown.sh
+
+rm -rf testrun-*/images.tar
+find testrun-* -maxdepth 0 -exec tar -czf {}.tar.gz {} \;
