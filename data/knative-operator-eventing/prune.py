6b6bf62c5ba520ead1b44baf59e8a4f62d75ce5b
Tyler Gu <jiaweig3@illinois.edu>
2022-11-27 15:15:03 -0600

    Update knative analysis

--- data/knative-operator-eventing/prune.py
+++ data/knative-operator-eventing/prune.py
@@ -1,5 +1,8 @@
 import input
 
 custom_fields = [
+    input.OverSpecifiedField(['spec', 'deployments', 'INDEX', 'tolerations'], True),
+    input.OverSpecifiedField(['spec', 'deployments', 'INDEX', 'affinity']),
+    
     input.ProblematicField(['spec', 'registry', 'override']),
 ]
--- data/knative-operator-serving/prune.py
+++ data/knative-operator-serving/prune.py
@@ -1,6 +1,9 @@
 import input
 
 custom_fields = [
+    input.OverSpecifiedField(['spec', 'deployments', 'INDEX', 'tolerations'], True),
+    input.OverSpecifiedField(['spec', 'deployments', 'INDEX', 'affinity']),
+
     input.ProblematicField(['spec', 'ingress', 'istio']),
     input.ProblematicField(['spec', 'registry', 'override']),
 ]
