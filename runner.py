e72b658564f24063c606f6d199150571863e6fca
Tyler Gu <jiaweig3@illinois.edu>
2023-03-24 07:12:59 -0500

    timeout

--- runner.py
+++ runner.py
@@ -129,11 +129,11 @@ class Runner(object):
         
         cmd = ['delete', '-f', mutated_filename, '-n', self.namespace]
         try:
-            cli_result = self.kubectl_client.kubectl(cmd, capture_output=True, text=True)
+            cli_result = self.kubectl_client.kubectl(cmd, capture_output=True, text=True, timeout=120)
         except subprocess.TimeoutExpired as e:
             logger.error('kubectl delete timeout.')
             return True
-            
+
         logger.debug('STDOUT: ' + cli_result.stdout)
         logger.debug('STDERR: ' + cli_result.stderr)
 
