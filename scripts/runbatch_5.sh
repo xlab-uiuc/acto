3ed443e51b3eb0eae1abe2580b883355ac142c21
Tyler Gu <jiaweig3@illinois.edu>
2023-03-31 12:06:06 -0500

    fix runbatch script

--- scripts/runbatch_5.sh
+++ scripts/runbatch_5.sh
@@ -1,11 +1,11 @@
 if [ $1 = "whitebox" ]
 then
-    python3 acto.py --config data/percona-xtradb-cluster-operator/config.json --num-workers 8 --num-cases 1 --workdir testrun-xtradb-whitebox --notify-crash
+    python3 acto.py --config data/percona-xtradb-cluster-operator/config.json --num-workers 12 --num-cases 1 --workdir testrun-xtradb-whitebox --notify-crash
     python3 checker.py --config data/percona-xtradb-cluster-operator/config.json --num-workers 16 --testrun-dir testrun-xtradb-whitebox
     bash scripts/teardown.sh
 elif [ $1 = "blackbox" ]
 then
-    python3 acto.py --config data/percona-xtradb-cluster-operator/config.json --blackbox --num-workers 8 --num-cases 1 --workdir testrun-xtradb-blackbox --notify-crash
+    python3 acto.py --config data/percona-xtradb-cluster-operator/config.json --blackbox --num-workers 12 --num-cases 1 --workdir testrun-xtradb-blackbox --notify-crash
     python3 checker.py --config data/percona-xtradb-cluster-operator/config.json --blackbox --num-workers 16 --testrun-dir testrun-xtradb-blackbox
     bash scripts/teardown.sh
 else
