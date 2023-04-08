a74e843e98a19d2c6293076f23e214a6e7c0eefc
Tyler Gu <jiaweig3@illinois.edu>
2023-04-07 20:14:40 -0500

    fix run script

--- scripts/runbatch_1.sh
+++ scripts/runbatch_1.sh
@@ -1,8 +1,9 @@
 if [ $1 = "whitebox" ]
-then 
+then
     python3 acto.py --config data/cass-operator/config.json --num-workers 16 --num-cases 1 --workdir testrun-cass-whitebox --notify-crash
     python3 checker.py --config data/cass-operator/config.json --num-workers 16 --testrun-dir testrun-cass-whitebox
     bash scripts/teardown.sh
+elif [ $1 = "blackbox" ]
 then
     python3 acto.py --config data/cass-operator/config.json --blackbox --num-workers 16 --num-cases 1 --workdir testrun-cass-blackbox --notify-crash
     python3 checker.py --config data/cass-operator/config.json --blackbox --num-workers 16 --testrun-dir testrun-cass-blackbox
