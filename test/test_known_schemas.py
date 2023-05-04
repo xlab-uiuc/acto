43238840a3827a9fea3bcaa3cfa834e28bc95ec8
Tyler Gu <jiaweig3@illinois.edu>
2023-05-04 15:51:27 -0500

    Add: crdb test for crdbop-920
    Signed-off-by: Tyler Gu <jiaweig3@illinois.edu>

--- test/test_known_schemas.py
+++ test/test_known_schemas.py
@@ -1,8 +1,11 @@
+import inspect
 import os
 import pathlib
 import sys
 import unittest
 import yaml
+from get_matched_schemas import field_matched, find_matched_schema
+import known_schemas
 
 sys.path.append('..')
 sys.path.append('.')
@@ -11,6 +14,7 @@ from known_schemas import *
 from schema import extract_schema
 
 test_dir = pathlib.Path(__file__).parent.resolve()
+
 
 class TestSchema(unittest.TestCase):
 
@@ -74,17 +78,31 @@ class TestSchema(unittest.TestCase):
             spec_schema = ObjectSchema(
                 ['root'], crd['spec']['versions'][0]['schema']['openAPIV3Schema']['properties']
                 ['spec']['properties']['override']['properties']['statefulSet']['properties']
-                ['spec']['properties']['template']['properties']['spec']['properties']['containers']['items'])
+                ['spec']['properties']['template']['properties']['spec']['properties']['containers']
+                ['items'])
 
         self.assertTrue(ContainerSchema.Match(spec_schema))
 
-        with open(os.path.join(test_dir, 'psmdb.percona.com_perconaservermongodbs.yaml'), 'r') as operator_yaml:
+        with open(os.path.join(test_dir, 'psmdb.percona.com_perconaservermongodbs.yaml'),
+                  'r') as operator_yaml:
             crd = yaml.load(operator_yaml, Loader=yaml.FullLoader)
             spec_schema = ObjectSchema(
                 ['root'], crd['spec']['versions'][-1]['schema']['openAPIV3Schema']['properties']
                 ['spec']['properties']['replsets']['items']['properties']['sidecars']['items'])
 
         self.assertTrue(ContainerSchema.Match(spec_schema))
+
+    def test_resources_match(self):
+        with open(os.path.join(test_dir, 'crdb_crd.yaml'), 'r') as operator_yaml:
+            crd = yaml.load(operator_yaml, Loader=yaml.FullLoader)
+            tls_schema = ObjectSchema(
+                ['root'],
+                crd['spec']['versions'][0]['schema']['openAPIV3Schema']['properties']['spec']
+                ['properties']['ingress']['properties']['sql']['properties']['tls']['items'])
+
+        self.assertTrue(IngressTLSSchema.Match(tls_schema))
+
+        self.assertTrue(field_matched(tls_schema, IngressTLSSchema))
 
     def test_find_matches(self):
         with open(os.path.join(test_dir, 'rabbitmq_crd.yaml'), 'r') as operator_yaml:
---
+++ test/utils.py
@@ -0,0 +1,23 @@
+import json
+import yaml
+
+from snapshot import Snapshot
+
+
+def construct_snapshot(trial_dir: str, generation: int):
+    mutated_path = f'{trial_dir}/mutated-{generation}.yaml'
+    operator_log_path = f'{trial_dir}/operator-{generation}.log'
+    system_state_path = "%s/system-state-%03d.json" % (trial_dir, generation)
+    cli_output_path = f'{trial_dir}/cli-output-{generation}.log'
+
+    with open(mutated_path, 'r') as mutated_file, \
+            open(operator_log_path, 'r') as operator_log_file, \
+            open(system_state_path, 'r') as system_state_file, \
+            open(cli_output_path, 'r') as cli_output_file:
+
+        mutated = yaml.load(mutated_file, Loader=yaml.FullLoader)
+        operator_log = operator_log_file.read().splitlines()
+        system_state = json.load(system_state_file)
+        cli_output = json.load(cli_output_file)
+
+        return Snapshot(mutated, cli_output, system_state, operator_log)
