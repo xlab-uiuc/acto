40cb84644209206353731e15af24a92b9633ac3e
wtj <zhangwt1997@gmail.com>
2023-07-27 10:23:46 +0800

    ansible/python: guide Ansible to use requirements.txt

--- scripts/ansible/configure.sh
+++ scripts/ansible/configure.sh
@@ -2,6 +2,7 @@
 ansible-playbook tmpfs.yaml -i ansible_hosts
 ansible-playbook go.yaml -i ansible_hosts
 ansible-playbook docker.yaml -i ansible_hosts
+ansible-playbook acto.yaml -i ansible_hosts
 ansible-playbook python.yaml -i ansible_hosts
 ansible-playbook kind.yaml -i ansible_hosts
 ansible-playbook kubectl.yaml -i ansible_hosts
@@ -10,4 +11,3 @@ ansible-playbook sysctl.yaml -i ansible_hosts
 ansible-playbook k3d.yaml -i ansible_hosts
 ansible-playbook k9s.yaml -i ansible_hosts
 ansible-playbook htop.yaml -i ansible_hosts
-ansible-playbook acto.yaml -i ansible_hosts
