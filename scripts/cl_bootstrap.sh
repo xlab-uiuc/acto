4a8828876008f0f2c0521a49a55eb4582d4fdb32
wtj <zhangwt1997@gmail.com>
2023-07-29 01:39:30 +0800

    cloudlab: add remaining instructions into the bootstrap script

--- scripts/cl_bootstrap.sh
+++ scripts/cl_bootstrap.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Get a sense of what's happenning at this timing
-echo hello      | sudo tee /etc/hello/hello
-whoami          | sudo tee /etc/hello/whoami
-pwd             | sudo tee /etc/hello/pwd
-ls /users       | sudo tee /etc/hello/users
-cat /etc/passwd | sudo tee /etc/hello/passwd
+#
+# Install Ansible
+#
 
 sudo apt update
 sudo apt -y install software-properties-common
@@ -13,3 +10,22 @@ sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt -y install ansible
 ansible-galaxy collection install ansible.posix
 ansible-galaxy collection install community.general
+
+#
+# Checkout the repository
+#
+
+cd /local/repository/
+git checkout sosp-ae
+
+#
+# Prepare the CloudLab machine(s) with Ansible
+#
+
+cd scripts/ansible/
+# By default the user will be the current one (geniuser)
+echo 127.0.0.1 > ansible_hosts
+# Work around the key authentication
+ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N "" && cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+sed -i 's|git@github.com:xlab-uiuc/acto.git|https://github.com/xlab-uiuc/acto.git|' acto.yaml # FIXME: see #247
+ansible-playbook -i ansible_hosts configure.yaml
