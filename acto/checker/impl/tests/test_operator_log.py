3254743d8f19c678c73003230e09874ece4b3c33
Xingjian Zhang <xingjian.zhang@qq.com>
2024-03-28 10:15:22 +0000

    Update test for new error log reporting

--- acto/checker/impl/tests/test_operator_log.py
+++ acto/checker/impl/tests/test_operator_log.py
@@ -24,13 +24,16 @@ def checker_func(s: Snapshot, prev_s: Snapshot) -> Optional[InvalidInputResult]:
         enumerate(
             [
                 InvalidInputResult(
+                    message="StatefulSet.apps \"test-cluster-server\" is invalid: spec.template.spec.restartPolicy: Unsupported value: \"OnFailure\": supported values: \"Always\"",
                     responsible_property=PropertyPath([]),
                 ),
                 None,
                 InvalidInputResult(
+                    message="tidb cluster acto-namespace/test-cluster is not valid and must be fixed first, aggregated error: spec.tidb.volumeName: Invalid value: \"lsgqejeydt\": Can not find volumeName: lsgqejeydt in storageVolumes or additionalVolumes/additionalVolumeMounts",
                     responsible_property=PropertyPath([]),
                 ),
                 InvalidInputResult(
+                    message="github.com/rabbitmq/cluster-operator/controllers.(*RabbitmqClusterReconciler).Reconcile\n\t/workspace/controllers/rabbitmqcluster_controller.go:260\nsigs.k8s.io/controller-runtime/pkg/internal/controller.(*Controller).reconcileHandler\n\t/go/pkg/mod/sigs.k8s.io/controller-runtime@v0.9.6/pkg/internal/controller/controller.go:298\nsigs.k8s.io/controller-runtime/pkg/internal/controller.(*Controller).processNextWorkItem\n\t/go/pkg/mod/sigs.k8s.io/controller-runtime@v0.9.6/pkg/internal/controller/controller.go:253\nsigs.k8s.io/controller-runtime/pkg/internal/controller.(*Controller).Start.func2.2\n\t/go/pkg/mod/sigs.k8s.io/controller-runtime@v0.9.6/pkg/internal/controller/controller.go:214",
                     responsible_property=PropertyPath(
                         [
                             "spec",
