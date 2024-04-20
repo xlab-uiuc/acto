2554d081bc0cb8e7358b058de02fa746a99b8140
Tyler Gu <jiaweig3@illinois.edu>
2024-04-19 23:52:25 -0500

    Fix scripts
    Signed-off-by: Tyler Gu <jiaweig3@illinois.edu>

--- anvil-ae-sampled.sh
+++ anvil-ae-sampled.sh
@@ -1,12 +1,12 @@
 echo "Running Performance Measurement workloads"
 
-python3 -m plugins.performance_measurement.measure_performance --workdir testrun-anvil-zk-performance --input-dir anvil-ae-data/testrun-anvil-zk --anvil-config data/anvil-zookeeper-operator/config.json --reference-config data/zookeeper-operator/v0.2.15/config.json --project zookeeper-operator --sample
+python3 -m plugins.performance_measurement.measure_performance --workdir testrun-anvil-zk-performance --input-dir anvil-ae-data/testrun-anvil-zk --anvil-config data/anvil-zookeeper-operator/config.json --reference-config data/zookeeper-operator/v0.2.15/config.json --project zookeeper-operator --sample 0.1
 echo "ZooKeeper operator performance results are stored in testrun-anvil-zk-performance"
 
-python3 -m plugins.performance_measurement.measure_performance --workdir testrun-anvil-rabbitmq-performance --input-dir anvil-ae-data/testrun-anvil-rabbitmq --anvil-config data/anvil-rabbitmq-operator/config.json --reference-config data/rabbitmq-operator/v2.5.0/config.json --project rabbitmq-operator --sample
+python3 -m plugins.performance_measurement.measure_performance --workdir testrun-anvil-rabbitmq-performance --input-dir anvil-ae-data/testrun-anvil-rabbitmq --anvil-config data/anvil-rabbitmq-operator/config.json --reference-config data/rabbitmq-operator/v2.5.0/config.json --project rabbitmq-operator --sample 0.1
 echo "RabbitMQ controller performance results are stored in testrun-anvil-rabbitmq-performance"
 
-python3 -m plugins.performance_measurement.measure_performance --workdir testrun-anvil-fluent-performance --input-dir anvil-ae-data/testrun-anvil-fluent --anvil-config data/anvil-fluent-controller/config.json --reference-config data/fluent-operator/config.json --project fluent-operator --sample
+python3 -m plugins.performance_measurement.measure_performance --workdir testrun-anvil-fluent-performance --input-dir anvil-ae-data/testrun-anvil-fluent --anvil-config data/anvil-fluent-controller/config.json --reference-config data/fluent-operator/config.json --project fluent-operator --sample 0.1
 echo "Fluent controller performance results are stored in testrun-anvil-fluent-performance"
 
 echo "Parsing performance data"
