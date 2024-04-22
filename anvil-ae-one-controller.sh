ad613793c43be78a1413bb21d05743c1deec3342
marshtompsxd <xudongsunmarshtomp@gmail.com>
2024-04-21 19:10:51 -0500

    Allow the bash scripts to take the sample rate as an argument

--- anvil-ae-one-controller.sh
+++ anvil-ae-one-controller.sh
@@ -1,6 +1,6 @@
 echo "Running Performance Measurement workloads for ZooKeeper controller"
 
-python3 -m plugins.performance_measurement.measure_performance --workdir testrun-anvil-zk-performance --input-dir anvil-ae-data/testrun-anvil-zk --anvil-config data/anvil-zookeeper-operator/config.json --reference-config data/zookeeper-operator/v0.2.15/config.json --project zookeeper-operator --sample 0.1
+python3 -m plugins.performance_measurement.measure_performance --workdir testrun-anvil-zk-performance --input-dir anvil-ae-data/testrun-anvil-zk --anvil-config data/anvil-zookeeper-operator/config.json --reference-config data/zookeeper-operator/v0.2.15/config.json --project zookeeper-operator --sample $1
 echo "ZooKeeper operator performance results are stored in testrun-anvil-zk-performance"
 
 echo "Parsing performance data"
