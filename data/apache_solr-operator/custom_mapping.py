8ed1c7f81196849992ec066c97baee2ef4472266
Tyler Gu <jiaweig3@illinois.edu>
2024-08-12 17:22:44 -0500

    Fix Kubernetes mapping
    Signed-off-by: Tyler Gu <jiaweig3@illinois.edu>

--- data/apache_solr-operator/custom_mapping.py
+++ data/apache_solr-operator/custom_mapping.py
@@ -5,4 +5,14 @@ KUBERNETES_TYPE_MAPPING: list[CustomKubernetesMapping] = [
         schema_path=["spec", "backupRepositories", "ITEM", "volume", "source"],
         kubernetes_schema_name="io.k8s.api.core.v1.Volume",
     ),
+    CustomKubernetesMapping(
+        schema_path=[
+            "spec",
+            "customSolrKubeOptions",
+            "podOptions",
+            "volumes",
+            "ITEM",
+        ],
+        kubernetes_schema_name="io.k8s.api.core.v1.Volume",
+    ),
 ]
