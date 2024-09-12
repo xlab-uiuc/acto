9009c33ed13f68ccd61069259e6a4dd0718917b5
yimingsu <yimingsu@node0.yimingsu-215888.sieve-acto-pg0.utah.cloudlab.us>
2024-09-12 16:21:59 -0600

    Improved crawl_example.py and config.json for cass-operator
    Now the script also crawls auxiliary yamls that are used to set up test yamls. It writes the auxiliary yamls into one big yaml called `aux-examples.yaml`
    
    Also, `config.json` for `cass-operator` now applies these aux yamls to prepare for testing.

--- scripts/crawl_examples.py
+++ scripts/crawl_examples.py
@@ -1,12 +1,21 @@
+"""Crawls yaml examples from target repo as testing material"""
+
 import argparse
-import glob, os
+import glob
+import os
 import yaml
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Crawl CR examples in the project repo')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Crawl CR examples in the project repo"
+    )
 
-    parser.add_argument('--dir', dest='dir', help='Project repo dir', required=True)
-    parser.add_argument('--kind', '-k', dest='kind', help='CR kind', required=True)
+    parser.add_argument(
+        "--dir", dest="dir", help="Project repo dir", required=True
+    )
+    parser.add_argument(
+        "--kind", "-k", dest="kind", help="CR kind", required=False
+    )
     # parser.add_argument('--dest',
     #                     dest='dest',
     #                     help='Directory to store the crawlled examples',
@@ -14,18 +23,52 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    results = []
+    main_results = []
+    aux_results = []
 
-    for file in glob.glob(os.path.join(args.dir, '**', '*.yaml'), recursive=True):
-        with open(file, 'r') as yaml_file:
+    for file in glob.glob(
+        os.path.join(args.dir, "**", "*.yaml"), recursive=True
+    ):
+        with open(file, "r", encoding="utf-8") as yaml_file:
             try:
                 file_content = yaml.load(yaml_file, Loader=yaml.FullLoader)
-            except:
+            except yaml.YAMLError as e:
                 continue
-            if 'kind' in file_content and file_content['kind'] == args.kind:
-                print(file)
-                results.append(file_content)
+            if "kind" in file_content:
+                try:
+                    if file_content["namespace"] != "":
+                        file_content.pop("namespace", None)
+                except KeyError as e:
+                    print("No namespace, no op")
 
-    with open('examples.yaml', 'w') as out_file:
-        yaml.dump_all(results, out_file)
+                try:
+                    if file_content["metadata"]["namespace"] != "":
+                        file_content["metadata"].pop("namespace", None)
+                except KeyError as e:
+                    print("No metadata.namespace, no op")
 
+                try:
+                    if file_content["spec"]["datacenter"]["namespace"] != "":
+                        file_content["spec"]["datacenter"].pop(
+                            "namespace", None
+                        )
+                except KeyError as e:
+                    print("No spec.datacenter.namespace, no op")
+
+                if file_content["kind"] == "Deployment":
+                    continue
+                elif file_content["kind"] == args.kind:
+                    print(file)
+                    main_results.append(file_content)
+                elif file_content["kind"] == "StorageClass":
+                    print(file)
+                    file_content["provisioner"] = "rancher.io/local-path"
+                    aux_results.append(file_content)
+                elif file_content["kind"] != "CustomResourceDefinition":
+                    print(file)
+                    aux_results.append(file_content)
+
+    with open("examples.yaml", "w", encoding="utf-8") as out_file:
+        yaml.dump_all(main_results, out_file)
+    with open("aux-examples.yaml", "w", encoding="utf-8") as out_file:
+        yaml.dump_all(aux_results, out_file)
