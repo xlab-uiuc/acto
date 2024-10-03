e878fc487e21ad3e0807c105222fc7433c7edfd9
Tyler Gu <jiaweig3@illinois.edu>
2024-10-03 13:56:50 -0500

    Include failure ratio in pod failure file name
    Signed-off-by: Tyler Gu <jiaweig3@illinois.edu>

--- chactos/failures/pod_failures_chaos.py
+++ chactos/failures/pod_failures_chaos.py
@@ -1,27 +1,27 @@
-from chactos.failures.failure import Failure
-
 import yaml
 
-FAILURE_DIR = ".failures"
-
+from chactos.failures.failure import Failure
 
 
 class PodFailure(Failure):
     """Simulates a pod crash failure in the cluster"""
 
     def __init__(
-        self, app_selector: dict, namespace: str, failure_ratio: float,
+        self,
+        app_selector: dict,
+        namespace: str,
+        failure_ratio: int,
     ):
         self.app_selector = app_selector
         self.namespace = namespace
         self.failure_ratio = failure_ratio
 
         super().__init__()
-    
+
     def name(self) -> str:
         """Get the name of the failure"""
-        return "pod-crash-failure"
-    
+        return "pod-crash-failure-" + str(self.failure_ratio)
+
     def to_dict(self) -> dict:
         """Dump the spec to a dict"""
         return {
@@ -29,17 +29,17 @@ class PodFailure(Failure):
             "kind": "PodChaos",
             "metadata": {
                 "name": "pod-crash-failure",
-                "namespace": "chaos-mesh"
+                "namespace": "chaos-mesh",
             },
             "spec": {
                 "action": "pod-failure",
                 "mode": "fixed-percent",
                 "value": self.failure_ratio,
-                "duration": '30s',
-                "selector": self.app_selector 
-            } 
+                "duration": "30s",
+                "selector": self.app_selector,
+            },
         }
-    
+
     def to_file(self, file_path: str):
         """Write the spec to a file"""
         with open(file_path, "w", encoding="utf-8") as f:
