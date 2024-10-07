f6eda21cb6be541dd0a4afb328aa6e158d717b87
Tyler Gu <jiaweig3@illinois.edu>
2024-10-07 16:25:25 -0500

    Fix Kubernetes engine
    Signed-off-by: Tyler Gu <jiaweig3@illinois.edu>

--- test/integration_tests/test_kubernetes_engines.py
+++ test/integration_tests/test_kubernetes_engines.py
@@ -5,10 +5,11 @@ import os
 import pytest
 
 # from acto.kubernetes_engine.base import KubernetesEngine
+from acto.kubernetes_engine.base import KubernetesEngine
 from acto.kubernetes_engine.kind import Kind
 from acto.kubernetes_engine.minikube import Minikube
 
-testcases = [("kind", 3, "v1.27.3")]
+testcases = [("kind", 4, "v1.27.3")]
 
 
 @pytest.mark.kubernetes_engine
@@ -18,6 +19,7 @@ def test_kubernetes_engines(cluster_type: str, num_nodes, version):
     config_path = os.path.join(os.path.expanduser("~"), ".kube/test-config")
     name = "test-cluster"
 
+    cluster_instance: KubernetesEngine
     if cluster_type == "kind":
         cluster_instance = Kind(
             acto_namespace=0, num_nodes=num_nodes, version=version
@@ -34,7 +36,7 @@ def test_kubernetes_engines(cluster_type: str, num_nodes, version):
     cluster_instance.create_cluster(name, config_path)
 
     node_list = cluster_instance.get_node_list(name)
-    assert len(node_list) == num_nodes
+    assert len(node_list) == num_nodes + 1
 
     cluster_instance.delete_cluster(name, config_path)
     with pytest.raises(RuntimeError):
