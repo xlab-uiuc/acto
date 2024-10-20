07f19ac5ece093ba10a5f91a3e4faec79e3f2ce1
TZ-zzz <tangzhen1027@gmail.com>
2024-10-19 20:22:49 -0500

    update the script for config values

--- scripts/gen_config.py
+++ scripts/gen_config.py
@@ -17,8 +17,6 @@ def read_config(path, file_name):
 
 def rec_search(prev_name, property):
     if property["type"] != "object":
-        if property["type"] == "boolean":
-            return {}
         return {prev_name: {"description": property["description"], "type": property["type"],"default": property["default"]}}
     else:
         leaf_properties = {}
@@ -37,53 +35,63 @@ def gen_values(properties, path, api_key, application):
         description = info["description"]
         datatype = info["type"]
         default = info["default"]
-
-        prop = f"property: {p}\n description: {description}\n type: {datatype}\n default: {default}\n"
-
-        prompt = "Here is the property that need values:\n"
-        prompt += f"{prop}\n"
-
-        prompt += "\nThe property has a datatype and description provided above, for valid values, please make sue that the values satisfy the datatype and description. For invalid value, please make sure it also satisfy the datatype but to be an erroneous one.\n"
-
-        prompt += "\n\nProvide two valid values for the property and one invalid value that satisfies the type, but to be an erroneous value\n. Directly give me the value in json format without any other message, for example\n"
-
-        format = f"property: {p}:\n"
-        format += f"  valid_value1: value1\n"
-        format += f"  valid_value2: value2\n"
-        format += f"  invalid_value: value3\n"
-
-        prompt += format
-
-        completion = openai.chat.completions.create(
-            model="gpt-4o",
-            messages=[
-                {"role": "system", "content": context},
-                {"role": "user", "content": prompt},
-            ],
-        )
-
-        result_text = completion.choices[0].message.content
-        print(result_text)
-
-        result = result_text.split("```json\n")[1].split("```")[0]
-
         output_file = os.path.join(path, f"value_{p}.json")
-        with open(output_file, "w") as f:
-            f.write(result)
+        if datatype == "boolean":
+            result = {p : {"valid_value1": True, "valid_value2": False, "invalid_value": "N/A"}}
+            with open(output_file, "w") as f:
+                json.dump(result, f, indent=4)
+
+        else:
+            continue
+            prop = f"property: {p}\n description: {description}\n type: {datatype}\n default: {default}\n"
+
+            prompt = "Here is the property that need values:\n"
+            prompt += f"{prop}\n"
+
+            prompt += "\nThe property has a datatype and description provided above, for valid values, please make sue that the values satisfy the datatype and description. For invalid value, please make sure it also satisfy the datatype but to be an erroneous one.\n"
+
+            prompt += "\n\nProvide two valid values for the property and one invalid value that satisfies the type, but to be an erroneous value\n. Please follow the following fomart strictly so that it can be write to the json file directly, for example\n"
+
+            format= "```json\n"
+            format += "{\n"
+            format += f"  \"{p}\": " +  "{\n"
+            format += f"    \"valid_value1\": value1\n"
+            format += f"    \"valid_value2\": value2\n"
+            format += f"    \"invalid_value\": value3"
+            format += "  }\n"
+            format += "}\n"
+            format += "```"
+
+            prompt += format
+
+            completion = openai.chat.completions.create(
+                model="gpt-4o",
+                messages=[
+                    {"role": "system", "content": context},
+                    {"role": "user", "content": prompt},
+                ],
+            )
+
+            result_text = completion.choices[0].message.content
+            
+
+            result = result_text.split("```json\n")[1].split("```")[0]
+            with open(output_file, "w") as f:
+                f.write(result)
 
 
-# def store_to_examples(path):
-#     main_results = []
+def collect_result(path, file_name):
+    main_results = {}
 
-#     for file in glob.glob(os.path.join(path, "*.yaml"), recursive=True):
-#         if file == "examples.yaml":
-#             continue
-#         with open(file, "r") as f:
-#             content = yaml.safe_load_all(f)
-#             for doc in content:
-#                 main_results.append(doc)
-#     with open(os.path.join(path, "examples.yaml"), "w") as f:
-#         yaml.dump_all(main_results, f)
+    for file in glob.glob(os.path.join(path, "*.json"), recursive=True):
+        if file == os.path.join(path, file_name):
+            continue
+        print(file)
+        with open(file, "r") as f:
+            content = json.load(f)
+            main_results.update(content)
+    with open(os.path.join(path, "config_values.json"), "w") as f:
+        json.dump(main_results, f, indent=4)
 
 
 if __name__ == "__main__":
@@ -104,4 +112,4 @@ if __name__ == "__main__":
     config_schema = read_config(args.path, args.file_name)
 
     gen_values(config_schema, args.path, args.api_key, args.application)
-    # store_to_examples(args.path)
+    collect_result(args.path, args.file_name)
