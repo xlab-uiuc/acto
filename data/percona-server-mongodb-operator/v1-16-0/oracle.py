ad9091f05cb458cdc5fc0f6bef8a43d3668c0b1e
TZ-zzz <tangzhen1027@gmail.com>
2024-11-14 14:53:41 -0600

    add custom oracle for mongodb

---
+++ data/percona-server-mongodb-operator/v1-16-0/oracle.py
@@ -0,0 +1,93 @@
+import base64
+from typing import Optional
+
+import kubernetes
+
+from acto.checker.checker import CheckerInterface
+from acto.oracle_handle import OracleHandle
+from acto.result import OracleResult
+from acto.snapshot import Snapshot
+from acto.utils.thread_logger import get_thread_logger
+from acto.checker.impl.state_compare import CustomCompareMethods
+import json
+import yaml
+import re
+
+
+class MongoDBConfigChecker(CheckerInterface):
+    """Custom oracle for checking Cassandra config"""
+
+    name = "mongodb-config-checker"
+
+    def __init__(self, oracle_handle: OracleHandle, **kwargs):
+        super().__init__(**kwargs)
+        self.oracle_handle = oracle_handle
+
+    def check(
+        self, generation: int, snapshot: Snapshot, prev_snapshot: Snapshot
+    ) -> Optional[OracleResult]:
+        """Check the Cassandra config"""
+        logger = get_thread_logger()
+
+        if (
+            "replsets" in snapshot.system_state["custom_resource_spec"]
+            and len(snapshot.system_state["custom_resource_spec"]["replsets"]) > 0
+            and "configuration" in snapshot.system_state["custom_resource_spec"]["replsets"][0]
+        ):
+            mongo_yaml = yaml.load(snapshot.system_state["custom_resource_spec"]["replsets"][
+                "configuration"], Loader=yaml.FullLoader)
+        else:
+            return None
+
+        sts_list = self.oracle_handle.get_stateful_sets()
+        pod_list = self.oracle_handle.get_pods_in_stateful_set(sts_list[0])
+
+        core_v1 = kubernetes.client.CoreV1Api(self.oracle_handle.k8s_client)
+
+        secret = core_v1.read_namespaced_secret(
+            "my-cluster-name-secrets", self.oracle_handle.namespace
+        ).data
+
+        username = base64.b64decode(secret["MONGODB_DATABASE_ADMIN_USER"]).decode("utf-8")
+        password = base64.b64decode(secret["MONGODB_DATABASE_ADMIN_PASSWORD"]).decode("utf-8")
+
+        pod = pod_list[0]
+        p = self.oracle_handle.kubectl_client.exec(
+            pod.metadata.name,
+            pod.metadata.namespace,
+            [
+                "mongosh",
+                "-u",
+                username,
+                "-p",
+                password,
+                "--eval",
+                "db.adminCommand({getCmdLineOpts: 1})",
+            ],
+            capture_output=True,
+            text=True,
+        )
+        if p.returncode != 0:
+            return OracleResult(message="MongoDB config check failed")
+
+        lines = p.stdout.split("\n")
+        lines = [x + "\n" for x in lines[15:]]
+
+        mark = 0
+        for i in range(len(lines)):
+            lines[i] = re.sub("(\w+):", '"\g<1>":', lines[i])
+            lines[i] = re.sub("'(.+)'", '"\g<1>"', lines[i])
+            if '\"ok\":' in lines[i]:
+                mark = i
+                break
+        lines[mark - 1] = re.sub("(\w*)},", '\g<1>}', lines[mark - 1])
+        config_data = json.loads("".join(lines[:mark] + ["}"]))["parsed"]
+
+        compare_methods = CustomCompareMethods()
+        if not compare_methods.equals(mongo_yaml, config_data):
+            return OracleResult(message="MongoDB config check failed due to missing keys or wrong values")
+
+        return None
+
+
+CUSTOM_CHECKER: list[type] = [MongoDBConfigChecker]
