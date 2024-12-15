8135171afb14a564f82074ed7f4e55a6eb8442be
Tyler Gu <jiaweig3@illinois.edu>
2024-12-14 18:24:18 -0600

    Update the run scripts
    Signed-off-by: Tyler Gu <jiaweig3@illinois.edu>

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 # Functionality tests
 python3 -m acto --config data/percona-server-mongodb-operator/v1-16-0/func-only.json --workdir testrun-mongodb-func --num-workers 4
 
-python3 -m acto --config data/cass-operator/v1-22/func-only.json --testrun-dir testrun-cass-func --num-workers 6
+python3 -m acto --config data/cass-operator/v1-22/func-only.json --workdir testrun-cass-func --num-workers 6
 
 python3 -m acto --config data/tidb-operator/v1-6-0/func-only.json --workdir testrun-tidb-func --num-workers 3
 
@@ -30,12 +30,19 @@ python3 -m chactos --config data/cass-operator/v1-22/func-only.json \
     --testrun-dir testrun-cass-func \
     --num-workers 6
 
+python3 -m chactos --config data/tidb-operator/v1-6-0/func-only.json \
+    --workdir testrun-tidb-func-fi \
+    --fi-config chactos/tidb-operator.json \
+    --testrun-dir testrun-tidb-func \
+    --num-workers 3
+
 python3 -m chactos --config data/mariadb-operator/v0-30-0/func-only.json \
     --workdir testrun-mariadb-func-fi \
     --fi-config chactos/mariadb-operator.json \
     --testrun-dir testrun-mariadb-func \
     --num-workers 6
 
+# Config fault injection run
 
 python3 -m chactos --config data/percona-server-mongodb-operator/v1-16-0/config-only.json \
     --workdir testrun-mongodb-conf-fi \
@@ -43,9 +50,20 @@ python3 -m chactos --config data/percona-server-mongodb-operator/v1-16-0/config-
     --testrun-dir testrun-mongodb-conf \
     --num-workers 4
 
+python3 -m chactos --config data/cass-operator/v1-22/config-only.json \
+    --workdir testrun-cass-conf-fi \
+    --fi-config chactos/cass-operator.json \
+    --testrun-dir testrun-cass-conf \
+    --num-workers 4
 
 python3 -m chactos --config data/tidb-operator/v1-6-0/config-only.json \
     --workdir testrun-tidb-conf-fi \
     --fi-config chactos/tidb-operator.json \
     --testrun-dir testrun-tidb-conf \
     --num-workers 3
+
+python3 -m chactos --config data/mariadb-operator/v0-30-0/config-only.json \
+    --workdir testrun-mariadb-conf-fi \
+    --fi-config chactos/mariadb-operator.json \
+    --testrun-dir testrun-mariadb-conf \
+    --num-workers 6
