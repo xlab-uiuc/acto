4ecc44a8990de1cfa0264bc56ec92ec894c79fb6
TZ-zzz <tangzhen1027@gmail.com>
2024-12-24 23:48:30 -0600

    rm invalid configurations

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,9 @@
 # Functionality tests
 python3 -m acto --config data/percona-server-mongodb-operator/v1-16-0/func-only.json --workdir testrun-mongodb-func --num-workers 4
 
-python3 -m acto --config data/cass-operator/v1-22/func-only.json --workdir testrun-cass-func --num-workers 6
+python3 -m acto --config data/cass-operator/v1-22/func-only.json --workdir testrun-cass-func --num-workers 4
 
-python3 -m acto --config data/tidb-operator/v1-6-0/func-only.json --workdir testrun-tidb-func --num-workers 3
+python3 -m acto --config data/tidb-operator/v1-6-0/func-only.json --workdir testrun-tidb-func --num-workers 4
 
 python3 -m acto --config data/mariadb-operator/v0-30-0/func-only.json --workdir testrun-mariadb-func --num-workers 6
 
@@ -11,9 +11,9 @@ python3 -m acto --config data/mariadb-operator/v0-30-0/func-only.json --workdir 
 # Config tests
 python3 -m acto --config data/percona-server-mongodb-operator/v1-16-0/config-only.json --workdir testrun-mongodb-conf --num-workers 4
 
-python3 -m acto --config data/cass-operator/v1-22/config-only.json --workdir testrun-cass-conf --num-workers 6
+python3 -m acto --config data/cass-operator/v1-22/config-only.json --workdir testrun-cass-conf --num-workers 4
 
-python3 -m acto --config data/tidb-operator/v1-6-0/config-only.json --workdir testrun-tidb-conf --num-workers 3
+python3 -m acto --config data/tidb-operator/v1-6-0/config-only.json --workdir testrun-tidb-conf --num-workers 4
 
 python3 -m acto --config data/mariadb-operator/v0-30-0/config-only.json --workdir testrun-mariadb-conf --num-workers 6
 
@@ -28,13 +28,13 @@ python3 -m chactos --config data/cass-operator/v1-22/func-only.json \
     --workdir testrun-cass-func-fi \
     --fi-config chactos/cass-operator.json \
     --testrun-dir testrun-cass-func \
-    --num-workers 6
+    --num-workers 4
 
 python3 -m chactos --config data/tidb-operator/v1-6-0/func-only.json \
     --workdir testrun-tidb-func-fi \
     --fi-config chactos/tidb-operator.json \
     --testrun-dir testrun-tidb-func \
-    --num-workers 3
+    --num-workers 4
 
 python3 -m chactos --config data/mariadb-operator/v0-30-0/func-only.json \
     --workdir testrun-mariadb-func-fi \
@@ -60,7 +60,7 @@ python3 -m chactos --config data/tidb-operator/v1-6-0/config-only.json \
     --workdir testrun-tidb-conf-fi \
     --fi-config chactos/tidb-operator.json \
     --testrun-dir testrun-tidb-conf \
-    --num-workers 3
+    --num-workers 4
 
 python3 -m chactos --config data/mariadb-operator/v0-30-0/config-only.json \
     --workdir testrun-mariadb-conf-fi \
