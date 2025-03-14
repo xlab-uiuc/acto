a964f76fb85f4fc7b9b143ddd5e5f6bd53a3714c
TZ-zzz <tangzhen1027@gmail.com>
2025-03-14 20:03:00 +0800

    make the custom_runner work

--- data/minio-operator/v7-0-0/custom_runner.py
+++ data/minio-operator/v7-0-0/custom_runner.py
@@ -9,7 +9,9 @@ from acto.schema.base import BaseSchema
 from acto.schema.under_specified import UnderSpecifiedSchema
 from acto.utils.thread_logger import get_thread_logger
 
+import base64
 import json
+import yaml
 
 NEXT_CONFIG: dict = {}
 SECRET_NAME: str = "storage-configuration"
@@ -26,10 +28,16 @@ class MinIOConfigSchema(UnderSpecifiedSchema):
         # Set the global variable
         global NEXT_CONFIG  # pylint: disable=global-statement
         NEXT_CONFIG = value
+        logger = get_thread_logger()
+        logger.info("encoding")
+        logger.info(NEXT_CONFIG)
         return SECRET_NAME
 
     def decode(self, value: str) -> dict:
-        return value
+        logger = get_thread_logger()
+        logger.info("decoding")
+        logger.info(value)
+        return NEXT_CONFIG
 
     @classmethod
     def from_original_schema(cls, original_schema: BaseSchema) -> Self:
@@ -54,14 +62,25 @@ def minio_config_hook(api_client: kubernetes.client.ApiClient) -> None:
 
     # Create Secret based on the global variable
     # NEXT_CONFIG
-    env_exports = "export MINIO_ROOT_USER=\"minio\"\nexport MINIO_ROOT_PASSWORD=\"minio123\"\nexport MINIO_BROWSER=\"on\"".join(f'export {key}="{value}"\n' for key, value in NEXT_CONFIG.items())
-
-    secret = { "config.env": env_exports }
+    env_exports = "export MINIO_ROOT_USER=\"minio\"\nexport MINIO_ROOT_PASSWORD=\"minio123\"\nexport MINIO_BROWSER=\"on\"\n"
+    for key, value in NEXT_CONFIG.items():
+        env_exports += f"export {key}={value}\n"
+    logger.info(env_exports)
+    print(env_exports)
+    # secret = { "config.env": base64.b64encode(env_exports.encode("ascii")).decode("ascii") }
+    secret = {
+        "apiVersion": "v1",
+        "kind": "Secret",
+        "stringData": { "config.env" : env_exports },
+        "metadata": {"name": "storage-configuration", "namespace": "minio-operator"},
+        "type": "Opaque"
+    }
     v1_api = kubernetes.client.CoreV1Api(api_client)
     try:
         v1_api.patch_namespaced_secret(name=SECRET_NAME, namespace="minio-operator", body=secret)
         logger.info(f"Secret '{SECRET_NAME}' patched successfully.")
         print(f"Secret '{SECRET_NAME}' patched successfully.")
+        print(v1_api.read_namespaced_secret(SECRET_NAME, namespace="minio-operator"))
     except ApiException as e:
         logger.info("Exception when calling CoreV1Api->patch_namespaced_resource_quota_status: %s\n" % e)
         print("Exception when calling CoreV1Api->patch_namespaced_resource_quota_status: %s\n" % e)
@@ -72,6 +91,6 @@ CUSTOM_RUNNER_HOOKS: list[RunnerHookType] = [minio_config_hook]
 
 CUSTOM_PROPERTY_SCHEMA_MAPPING = [
     CustomPropertySchemaMapping(
-        schema_path=["spec", "configuration"], custom_schema=MinIOConfigSchema
+        schema_path=["spec", "configuration", "name"], custom_schema=MinIOConfigSchema
     )
 ]
