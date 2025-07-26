bbff9c78bd42bb3a5102cc8a4ea62b6bfee40c4d
TZ-zzz <tangzhen1027@gmail.com>
2025-07-26 08:08:59 +0800

    reproduce mariadb-1022

--- test/oat_ae_utils.py
+++ test/oat_ae_utils.py
@@ -216,6 +216,7 @@ ALL_BUGS: dict[OperatorPrettyName, dict[str, OatBugConfig]] = {
         "mariadbop-1022": OatBugConfig(
             category=BugCategory.OPERATION_SEMANTICS,
             path="test/oat_tests/mariadbop-1022",
+            difftest=True,
         ),
         "mariadbop-1023": OatBugConfig(
             category=BugCategory.ERROR_HANDLING,
